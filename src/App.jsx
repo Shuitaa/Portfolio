fbc460bccbdeb649e63327eacd5c701b51f37976
Pierre <pierre.averty@gmail.com>
2021-04-21 00:17:38 +0200

    Create SearchInpute component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import SearchInput from './components/SearchInput';
+
+function App() {
+  return (
+    <SearchInput></SearchInput>
+  );
+}
+
+export default App;
---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,15 @@
+function SearchInput() {
+  return (
+    <div>
+      <input type="text" />
+      <div>
+        <div>
+          <span></span>
+          <span>Search</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchInput;
