05a346a1c281c5b6913b166989a9a1431494fcdf
Pierre Averty <pierre.averty@kering.com>
2021-05-10 16:57:35 +0200

    End slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,67 @@
+import { useEffect, useRef, useState } from 'react';
 import SliderItem from './SliderItem'
 
 function Slider(){
+
+    let list = useRef()
+
+    let [val, setVal] = useState(0);
+    let [status, setStatus] = useState("right")
+
+    useEffect(()=>{
+
+    }, [status]);
+
+    function processDistance(){
+        let lastChild = list.current.lastChild;
+        let dist = lastChild.offsetLeft - (614 - (lastChild.offsetWidth + 20));
+        let click = Math.ceil(dist/((dist*18.85)/100));
+
+        return dist/click;
+    }
+
+    function onSlide(event){
+        const lastChildLeft = list.current.lastChild.offsetLeft;
+        const listSize = list.current.offsetWidth;
+
+        setVal(event.target.classList.contains("slider__icon-left") 
+            ? val+=processDistance()
+            : val-=processDistance());
+
+        if(val === 0) setStatus("right")
+        else if((-val + listSize) >= lastChildLeft) setStatus("left")
+        else setStatus("both")
+            
+        list.current.style.transition = "transform 0.6s ease";
+        list.current.style.transform = `translateX(${val}px)`;
+
+        return true;
+    }
+
+
     return (
         <div className="slider">
-            <ul>
+            {(status === "left" || status ==="both")
+                && <div className="slider__icon-left" onClick={onSlide}></div>}
+            <ul ref={list}>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
+                <SliderItem></SliderItem>
                 <SliderItem></SliderItem>
                 <SliderItem></SliderItem>
                 <SliderItem></SliderItem>
@@ -16,7 +74,8 @@ function Slider(){
                 <SliderItem></SliderItem>
 
             </ul>
-            <div className="slider__icon-right"></div>
+            {(status === "right" || status ==="both")
+                && <div className="slider__icon-right" onClick={onSlide}></div>}
         </div>
     )
 }
--- src/components/Slider/SliderItem.jsx
+++ src/components/Slider/SliderItem.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useRef } from "react";
+
 function SliderItem(){
+
+    let item = useRef()
+
+    useEffect(()=>{
+        if(item.current.offsetLeft < 614 - (item.current.offsetWidth + 20))
+            console.log(true);
+    }, [])
+
     return (
-        <li className="slideritem">
+        <li className="slideritem" ref={item} >
             <button className="slideritem__container">
                 <div className="slideritem__img">
                     <img src="dino.JPG" alt="" />
                 </div>
-                <span>Dinoeazezaezakorofkazok</span>
+                <span>dinoeazezaezakorofkazok</span>
             </button>
         </li>
     )
